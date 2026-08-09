ff95395783d302ff93d10f42a5d8394702ee31db
vxmps.d0ll <hirakulee890@gmail.com>
2026-08-09 16:42:34 -0600

    MAIN MENU 1.0

---
+++ learn-to-pay/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_options_button_pressed():
+	get_tree().change_scene_to_file("res://options.tscn")
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+
+func _on_exit_button_pressed():
+	get_tree().quit()
